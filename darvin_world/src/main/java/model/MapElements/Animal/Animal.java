e83a3a343cca42609d017bd500a42755c4fb6f0c
Michał Gontarz <micgontarz@student.agh.edu.pl>
2025-01-20 13:47:51 +0100

    Poprawione EatenPlants

--- darvin_world/src/main/java/model/MapElements/Animal/Animal.java
+++ darvin_world/src/main/java/model/MapElements/Animal/Animal.java
@@ -107,6 +107,10 @@ public abstract class Animal implements MapElement {
         return this.eatenPlants;
     }
 
+    public void incrementEatenPlants() {
+        this.eatenPlants++;
+    }
+
     public boolean isAt(Vector2d position) {
         return this.position.equals(position);
     }
